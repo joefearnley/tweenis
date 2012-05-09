18c80a919c74e58ff22cd634c21af7db4f019c98
Joe Fearnley <joe.fearnley@gmail.com>
2012-05-08 20:13:20 -0400

    updated some javascript formatting

--- js/tweenis.js
+++ js/tweenis.js
@@ -60,21 +60,21 @@ function getTweetContent(tweet) {
  * http://www.simonwhatley.co.uk/examples/twitter/prototype/
  */
 String.prototype.parseURL = function() {
-	return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) {
-		return url.link(url);
-	});
+  return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) {
+    return url.link(url);
+  });
 };
 
 String.prototype.parseUsername = function() {
-	return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
-		var username = u.replace("@","")
-		return u.link("http://twitter.com/"+username);
-	});
+  return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+    var username = u.replace("@","")
+    return u.link("http://twitter.com/"+username);
+  });
 };
 
 String.prototype.parseHashtag = function() {
-	return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
-		var tag = t.replace('#','%23')
-		return t.link('http://search.twitter.com/search?q='+tag);
-	});
+  return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+    var tag = t.replace('#','%23')
+    return t.link('http://search.twitter.com/search?q='+tag);
+  });
 };
