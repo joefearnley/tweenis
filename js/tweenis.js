d6a9bbf17cbdf20caf433458723df0c8a919a8f7
Joe Fearnley <joe.fearnley@gmail.com>
2012-09-26 15:26:38 -0400

    Refactoring main js file.

--- js/tweenis.js
+++ js/tweenis.js
@@ -1,48 +1,15 @@
-/**
- * All the javascript for tweenis.net.
- */
-
 $(function() {
 
-  $.getJSON('http://search.twitter.com/search.json?q=penis&rpp=10&callback=?',
-    function(response) {
-      if(response.error) {
-        $('#tweets').html(Mustache.to_html(errorTemplate, response));
-        return false;
-      }
-
-      $.each(response.results, function(i, tweet) {
-        tweet.text = $.linkify(tweet.text);
-        $('#tweets').prepend(Mustache.to_html(tweetTemplate, tweet));
-      });
-    });
-
-  var interval = setInterval(function() {
-    var latestTweetId = $('#tweets').find('blockquote:first').attr('id');
-    var earliestTweetId = $('#tweets').find('blockquote:last').attr('id');
-
-    $.getJSON('http://search.twitter.com/search.json?q=penis&rpp=1&since_id='+latestTweetId+'&callback=?',
-      function(response) {
-        if(response.error) {
-          $('#tweets').html(Mustache.to_html(errorTemplate, response));
-          return false;
-        }
-
-        $.each(response.results, function(i, tweet) {
-          tweet.text = $.linkify(tweet.text);
-          $('#tweets').prepend(Mustache.to_html(tweetTemplate, tweet));
-          $('#'+tweet.id).hide().show('slow');
-        });
-      });
-  }, 5000);
-/*
   var TwitterCall = function(data) {
     var tweetTemplate = $('#tweetTemplate').html();
     var errorTemplate =  $('#errorTemplate').html();
 
-    var seachParams = '';
+    var searchParameters = '';
+    $.each(data, function(key, value) {
+      searchParameters += key + '=' + value + '&';
+    });
 
-    $.getJSON('http://search.twitter.com/search.json?q=penis&rpp=10&callback=?',
+    $.getJSON('http://search.twitter.com/search.json?' + searchParameters + '&callback=?',
       function(response) {
         if(response.error) {
           $('#tweets').html(Mustache.to_html(errorTemplate, response));
@@ -53,11 +20,24 @@ $(function() {
           tweet.text = $.linkify(tweet.text);
           $('#tweets').prepend(Mustache.to_html(tweetTemplate, tweet));
 
-          if(searchParms.indexOf('since_id') !== -1) {
+          if(searchParameters.indexOf('since_id') !== -1) {
             $('#'+tweet.id).hide().show('slow');
           }
         });
       });
   };
-*/
+
+  var queryData = {
+    'q': 'penis',
+    'rrp': 10
+  };
+
+  var call = new TwitterCall(queryData);
+
+  var interval = setInterval(function() {
+    queryData.since_id = $('#tweets').find('blockquote:first').attr('id');
+    queryData.rpp = 1;
+    var call = new TwitterCall(queryData);
+  }, 5000);
+
 });
