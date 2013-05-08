e414eaf876df9d71b05071ffbd902afde9893487
Joe Fearnley <joe.fearnley@gmail.com>
2013-05-07 22:49:30 -0400

    Updating linkify jquery plugin

--- js/jquery.linkify.js
+++ js/jquery.linkify.js
@@ -1,40 +1,46 @@
-/*
- * Linkify jQuery Plugin v1.0.0
- *
- * Copyright (c) 2011 Dobot (http://dobot.github.com/)
- * Licensed under the MIT license.
- *
- */
+function linkify(string, buildHashtagUrl, includeW3, target) {
+  string = string.replace(/((http|https|ftp)\:\/\/|\bw{3}\.)[a-z0-9\-\.]+\.[a-z]{2,3}(:[a-z0-9]*)?\/?([a-z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~])*/gi, function(captured) {
+    var uri;
+    if (captured.toLowerCase().indexOf("www.") == 0) {
+      if (!includeW3) {
+        return captured;
+      }
+      uri = "http://" + captured;
+    } else {
+      uri = captured;
+    }
+    return "<a href=\"" + uri+ "\" target=\"" + target + "\">" + captured + "</a>";;
+  });
+  
+  if (buildHashtagUrl) {
+    string = string.replace(/\B#(\w+)/g, "<a href=" + buildHashtagUrl("$1") +" target=\"" + target + "\">#$1</a>");
+  }
+  return string;
+}
 
-(function($){
-	$.extend({
-		linkify: function(str, options) {
-			var defaults = {
-				link: {
-					regex: /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,
-					template: "<a href=\"$1\">$1</a>"
-				},
-				user: {
-					regex: /(^|\s)@(\w+)/g,
-					template: '$1<a href="http://twitter.com/#!/$2">@$2</a>'
-				},
-				hash: {
-					regex: /(^|\s)#(\w+)/g,
-					template: '$1<a href="http://twitter.com/#!/search?q=%23$2">#$2</a>'
-				},
-				email: {
-					regex: /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi,
-					template: '<a href=\"mailto:$1\">$1</a>'
-				}
-			};
-
-			var types = $.extend(defaults, options);
-
-			$.each(types, function(name, type) {
-				str = str.replace(type.regex, type.template);
-			});
-
-			return str;
-		}
-	});
+(function($) {
+  $.fn.linkify = function(opts) {
+    return this.each(function() {
+      var $this = $(this);
+      var buildHashtagUrl;
+      var includeW3 = true;
+      var target = '_self';
+      if (opts) {
+        if (typeof opts  == "function") {
+          buildHashtagUrl = opts;
+        } else {
+          if (typeof opts.hashtagUrlBuilder == "function") {
+            buildHashtagUrl = opts.hashtagUrlBuilder;
+          }
+          if (typeof opts.includeW3 == "boolean") {
+            includeW3 = opts.includeW3;
+          }
+          if (typeof opts.target == "string") {
+            target = opts.target;
+          }
+        }
+      }
+      $this.html(linkify($this.html(), buildHashtagUrl, includeW3, target));
+    });
+  }
 })(jQuery);
